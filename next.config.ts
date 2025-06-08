72a7da0df26b691e46a1993384fdb86cfe100a54
Christopher Sancho <sanchochris@gmail.com>
2025-06-07 21:01:10 -0400

    Add Netlify configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'images.ctfassets.net'],
+    unoptimized: process.env.NODE_ENV === 'production',
+  },
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
